5c76919e8adffa5357617a93267171accc282054
jchxu <jchxu@qq.com>
2018-09-29 09:53:34 +0800

    2222

--- MainContract_LastOnePerMin_sorted.py
+++ MainContract_LastOnePerMin_sorted.py
@@ -9,7 +9,7 @@ tempfilename = filename.split('.')[0]+"_temp.csv"
 sortedfilename = filename.split('.')[0]+'_Sorted.csv'
 
 df = pd.read_csv(filename,encoding='gbk', dtype=str)
-df = df[:1000]
+#df = df[:1000]
 df2 = df.sort_index(ascending=False)    #数据倒序，变为查找相同分钟的第一个数据
 tempfile = open(tempfilename,mode='w')
 print('contract','datetime','lp',sep=',',file=tempfile)
@@ -23,6 +23,7 @@ for index,row in df2.iterrows():
     if not (recdt in existdts):
         existdts.append(recdt)
         print(row.contract, recdt, row.lp, sep=',',file=tempfile)
+        tempfile.flush()
 tempfile.close()    #将倒序且含唯一分钟时间的数据保存为临时文件
 
 data = pd.read_csv(tempfilename)
